8bd04bd3a6acffbe456b668de1f3cad358cf2e5a
Varun Shenoy <vnshenoy@stanford.edu>
2023-08-01 17:30:46 -0700

    updated truss for sdxl

--- stable-diffusion-xl-1.0/model/model.py
+++ stable-diffusion-xl-1.0/model/model.py
@@ -34,8 +34,6 @@ class Model:
         )
         self.refiner.to("cuda")
 
-
-
     def convert_to_b64(self, image: Image) -> str:
         buffered = BytesIO()
         image.save(buffered, format="JPEG")
@@ -45,13 +43,16 @@ class Model:
     def predict(self, model_input: Any) -> Any:
         prompt = model_input.pop("prompt")
         use_refiner = model_input.pop("use_refiner", False)
+        batch_size = model_input.pop("batch_size", 4)
 
-        images = self.pipe(prompt=[prompt] * 4, num_inference_steps=25).images # note: you can decrease num steps if using refiner
+
+
+        images = self.pipe(prompt=[prompt] * batch_size, num_inference_steps=25).images # note: you can decrease num steps if using refiner
 
         negative_prompt = "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, bad anatomy, watermark, signature, cut off"
 
         if use_refiner:
-            images = self.refiner(prompt=[prompt] * 4, negative_prompt=[negative_prompt] * 4, image=images).images # this is a list
+            images = self.refiner(prompt=[prompt] * batch_size, negative_prompt=[negative_prompt] * batch_size, image=images).images # this is a list
                 
         
         b64_results = [self.convert_to_b64(image) for image in images]
