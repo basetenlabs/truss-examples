7885883c713c87848c7bd97cbeb1f260e833a7c3
Bola Malek <bola@baseten.co>
2023-08-11 21:23:07 -0700

    Pin version and decode latents through VAE

--- sdxl-1.0-stream/model/model.py
+++ sdxl-1.0-stream/model/model.py
@@ -46,21 +46,30 @@ class Model:
         prompt = model_input.pop("prompt")
         use_refiner = model_input.pop("use_refiner", False)
         vae = self.pipe.vae
+        image_processor = self.pipe.image_processor
+        
         
         q = Queue() # `latents_callback`` produces, the `inner_cosumer`` consumes
         job_done = object() # signals the processing is done
     
         def latents_callback(i, t, latents):
-            outputRs = latents.permute(0, 2, 3, 1)
-            k = outputRs.cpu().detach().numpy()
-            size = latents.shape[0]
-            res = k[0]
-            image = Image.fromarray(np.uint8(res)).convert('RGB')
+            if i < 25:
+                return
+            
+            # make sure the VAE is in float32 mode, as it overflows in float16
+            if vae.dtype == torch.float16 and vae.config.force_upcast:
+                self.pipe.upcast_vae()
+                latents = latents.to(next(iter(vae.post_quant_conv.parameters())).dtype)
+
+            image = vae.decode(latents / vae.config.scaling_factor, return_dict=False)[0]
+
+            image = image_processor.postprocess(image, output_type="pil")[0]
+            
             b64_img = self.convert_to_b64(image)
             q.put(b64_img + "\n")
         
         def generation_task():
-            image = self.pipe(prompt=prompt, output_type="latent" if use_refiner else "pil", callback=latents_callback, callback_steps=15).images[0]
+            image = self.pipe(prompt=prompt, output_type="latent" if use_refiner else "pil", callback=latents_callback, callback_steps=5).images[0]
             
             if use_refiner:
                 image = self.refiner(prompt=prompt, image=image[None, :]).images[0]
@@ -79,7 +88,6 @@ class Model:
                 next_item = q.get(True, timeout=300) # Blocks until an input is available
                 if next_item is job_done:
                     break
-                logging.warn(len(next_item))
                 yield next_item
             thread.join()
             
