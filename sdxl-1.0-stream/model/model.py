fcab591fcb643cb47c330e45876f87c35b669926
Bola Malek <bola@baseten.co>
2023-08-16 10:29:13 -0700

    Move image conversion outside of callback

--- sdxl-1.0-stream/model/model.py
+++ sdxl-1.0-stream/model/model.py
@@ -53,23 +53,19 @@ class Model:
         job_done = object() # signals the processing is done
     
         def latents_callback(i, t, latents):
-            if i < 25:
+            if i < 20:
                 return
-            
             # make sure the VAE is in float32 mode, as it overflows in float16
             if vae.dtype == torch.float16 and vae.config.force_upcast:
                 self.pipe.upcast_vae()
                 latents = latents.to(next(iter(vae.post_quant_conv.parameters())).dtype)
 
-            image = vae.decode(latents / vae.config.scaling_factor, return_dict=False)[0]
+            image = vae.decode(latents / vae.config.scaling_factor, return_dict=False)[0].cpu().detach()
 
-            image = image_processor.postprocess(image, output_type="pil")[0]
-            
-            b64_img = self.convert_to_b64(image)
-            q.put(b64_img + "\n")
+            q.put(image)
         
         def generation_task():
-            image = self.pipe(prompt=prompt, output_type="latent" if use_refiner else "pil", callback=latents_callback, callback_steps=5).images[0]
+            image = self.pipe(prompt=prompt, output_type="latent" if use_refiner else "pil", callback=latents_callback, callback_steps=10,).images[0]
             
             if use_refiner:
                 image = self.refiner(prompt=prompt, image=image[None, :]).images[0]
@@ -85,10 +81,17 @@ class Model:
             import time
             while True:
                 time.sleep(0.005)
-                next_item = q.get(True, timeout=300) # Blocks until an input is available
-                if next_item is job_done:
+                image = q.get(True, timeout=300) # Blocks until an input is available
+                if image is job_done:
                     break
-                yield next_item
+                if  isinstance(image, str):
+                    yield image
+                    continue
+                    
+                image = image_processor.postprocess(image, output_type="pil")[0]
+                
+                b64_img = self.convert_to_b64(image)
+                yield b64_img + "\n"
             thread.join()
             
         return inner_consumer()
