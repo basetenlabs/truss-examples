d9e7b0a515c0fb1dddbb4eb9e4f273462f0048eb
Abu Qader <48742992+aspctu@users.noreply.github.com>
2023-09-27 10:34:36 -0500

    init

---
+++ wizard-vllm/model/model.py
@@ -0,0 +1,29 @@
+
+from typing import Any
+import os
+from vllm import SamplingParams
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.engine.async_llm_engine import AsyncLLMEngine
+import uuid
+
+class Model:
+    def __init__(self, **kwargs) -> None:
+        self.model_args = None
+        self.llm_engine = None
+
+    def load(self) -> None:
+        self.model_args = AsyncEngineArgs(model="TheBloke/wizardLM-7B-HF")
+        self.llm_engine = AsyncLLMEngine.from_engine_args(self.model_args)
+
+    async def predict(self, request: dict) -> Any:
+        prompt = request.pop('prompt')
+        sampling_params = SamplingParams(**request)
+        idx = str(uuid.uuid4().hex)
+        generator = self.llm_engine.generate(prompt, sampling_params, idx)
+        prev = ""
+        async for output in generator:
+            yield output
+            curr = output.outputs[0].text
+            delta = curr[len(prev):]
+            prev = curr
+            yield delta
