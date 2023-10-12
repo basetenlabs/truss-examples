379de91497735f34143090a114a248012e5b154b
Sid Shanker <sid.shanker@baseten.co>
2023-10-12 18:58:49 -0400

    Add cached weights model.

--- 6_high_performance/cached-weights/model/model.py
+++ 6_high_performance/cached-weights/model/model.py
@@ -1,32 +1,56 @@
-"""
-The `Model` class is an interface between the ML model that you're packaging and the model
-server that you're running it on.
+# In this example, we go through a Truss that serves an LLM, and _caches_ the weights
+# at build time. Loading model weights for any model can often be the most time-consuming
+# part of starting a model. Caching the weights at build time means that the weights
+# will be baked into the Truss image, and will be available _immediately_ when your model
+# replica starts. This means that cold starts will be _significantly faster_ with this approach.
+#
+# # Implementing the `Model` class
+#
+# With weight caching, you don't have to change anything about how the `Model` class
+# is implemented to take advantage of the weight caching.
+from typing import Dict, List
 
-The main methods to implement here are:
-* `load`: runs exactly once when the model server is spun up or patched and loads the
-   model onto the model server. Include any logic for initializing your model, such
-   as downloading model weights and loading the model into memory.
-* `predict`: runs every time the model server is called. Include any logic for model
-  inference and return the model output.
+import torch
+from transformers import LlamaForCausalLM, LlamaTokenizer
 
-See https://truss.baseten.co/quickstart for more.
-"""
+DEFAULT_SYSTEM_PROMPT = "You are a helpful, respectful and honest assistant."
+
+B_INST, E_INST = "[INST]", "[/INST]"
+B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+CHECKPOINT = "NousResearch/Llama-2-7b-chat-hf"
+
+
+def format_prompt(prompt: str, system_prompt: str = DEFAULT_SYSTEM_PROMPT) -> str:
+    return f"{B_INST} {B_SYS} {system_prompt} {E_SYS} {prompt} {E_INST}"
 
 
 class Model:
-    def __init__(self, **kwargs):
-        # Uncomment the following to get access
-        # to various parts of the Truss config.
-
-        # self._data_dir = kwargs["data_dir"]
-        # self._config = kwargs["config"]
-        # self._secrets = kwargs["secrets"]
-        self._model = None
+    def __init__(self, **kwargs) -> None:
+        self.model = None
+        self.tokenizer = None
 
     def load(self):
-        # Load model here and assign to self._model.
-        pass
+        self.model = LlamaForCausalLM.from_pretrained(
+            CHECKPOINT,
+            torch_dtype=torch.float16,
+            device_map="auto"
+        )
+        self.tokenizer = LlamaTokenizer.from_pretrained(
+            CHECKPOINT
+        )
 
-    def predict(self, model_input):
-        # Run model inference here
-        return model_input
+    def predict(self, request: Dict) -> Dict[str, List]:
+        prompt = request.pop("prompt")
+        inputs = self.tokenizer(format_prompt(prompt), return_tensors="pt")
+
+        outputs = self.model.generate(
+            **inputs,
+            do_sample=True,
+            num_beams=1,
+            max_new_tokens=100
+        )
+        response = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+
+        return {"response": response}
+
+
