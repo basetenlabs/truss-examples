8e3dea854afde5ef8250f72405af27a41557bfb6
Bola Malek <bola@baseten.co>
2023-10-24 10:51:40 -0700

    Fix lint

--- segment-anything/model/model.py
+++ segment-anything/model/model.py
@@ -8,6 +8,7 @@ from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 BASE64_PREAMBLE = "data:image/png;base64,"
 import logging
 
+
 class Model:
     def __init__(self, **kwargs) -> None:
         self._data_dir = kwargs["data_dir"]
@@ -21,16 +22,16 @@ class Model:
             checkpoint=str(self._data_dir / "sam_vit_h_4b8939.pth")
         )
         sam.to("cuda")
-        
+
         self._model = SamAutomaticMaskGenerator(sam, output_mode="coco_rle")
 
     def predict(self, model_input: Any) -> Any:
         input_image_url = model_input["image_url"]
-        
+
         req = urllib.request.urlopen(input_image_url)
         arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
         input_image_cv2 = cv2.imdecode(arr, -1)
-        
+
         masks = self._model.generate(input_image_cv2)
         result = {"masks": masks}
 
