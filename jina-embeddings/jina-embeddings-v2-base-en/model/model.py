b8d902e2a50fde44c6af6e206392501f0a06e780
Philip Kiely - Baseten <98474633+philipkiely-baseten@users.noreply.github.com>
2023-10-27 12:10:07 -0500

    Add Jina Embedding model (#52)
    Add a Truss for Jina embedding model, intending to add to model library
    today + publish a blog post on this

---
+++ jina-embeddings/jina-embeddings-v2-base-en/model/model.py
@@ -0,0 +1,20 @@
+from transformers import AutoModel
+
+
+class Model:
+    def __init__(self, **kwargs):
+        self._model = None
+
+    def load(self):
+        self._model = AutoModel.from_pretrained(
+            "jinaai/jina-embeddings-v2-base-en",
+            revision="0f472a4cde0e6e50067b8259a3a74d1110f4f8d8",
+            trust_remote_code=True,
+        )  # Version is pinned to prevent malicious code execution
+
+    def predict(self, model_input):
+        if "max_length" not in model_input.keys():
+            model_input["max_length"] = 8192
+        return self._model.encode(
+            model_input["text"], max_length=model_input["max_length"]
+        )
