442c89bbb8f212c9aa5dd0ee2d458b520705abb2
Timur Abishev <timur.abishev@baseten.co>
2023-11-17 16:25:51 -0500

    simplify code

--- mistral/mistral-7b-instruct-vllm/model/model.py
+++ mistral/mistral-7b-instruct-vllm/model/model.py
@@ -17,7 +17,11 @@ class Model:
         self.model_args = AsyncEngineArgs(model=self.repo_id)
         self.llm_engine = AsyncLLMEngine.from_engine_args(self.model_args)
 
-    def preprocess(self, request: dict):
+    async def predict(self, request: dict) -> Any:
+        prompt = request.pop("prompt")
+        stream = request.pop("stream", True)
+        formatted_prompt = self.prompt_format.replace("{prompt}", prompt)
+
         generate_args = {
             "n": 1,
             "best_of": 1,
@@ -29,30 +33,9 @@ class Model:
             "presence_penalty": 1.0,
             "use_beam_search": False,
         }
-        if "max_tokens" in request.keys():
-            generate_args["max_tokens"] = request["max_tokens"]
-        if "temperature" in request.keys():
-            generate_args["temperature"] = request["temperature"]
-        if "top_p" in request.keys():
-            generate_args["top_p"] = request["top_p"]
-        if "top_k" in request.keys():
-            generate_args["top_k"] = request["top_k"]
-        if "frequency_penalty" in request.keys():
-            generate_args["frequency_penalty"] = request["frequency_penalty"]
-        if "presence_penalty" in request.keys():
-            generate_args["presence_penalty"] = request["presence_penalty"]
-        if "use_beam_search" in request.keys():
-            generate_args["use_beam_search"] = request["use_beam_search"]
-        if "ignore_eos" in request.keys():
-            generate_args["ignore_eos"] = request["ignore_eos"]
-        request = {**request, **generate_args}
-        return request
-
-    async def predict(self, request: dict) -> Any:
-        prompt = request.pop("prompt")
-        stream = request.pop("stream", True)
-        formatted_prompt = self.prompt_format.replace("{prompt}", prompt)
-        sampling_params = SamplingParams(**request)
+        generate_args.update(request)
+        
+        sampling_params = SamplingParams(**generate_args)
         idx = str(uuid.uuid4().hex)
         vllm_generator = self.llm_engine.generate(
             formatted_prompt, sampling_params, idx
@@ -72,4 +55,4 @@ class Model:
             full_text = ""
             async for delta in generator():
                 full_text += delta
-            return {"result": full_text}
+            return {"text": full_text}
