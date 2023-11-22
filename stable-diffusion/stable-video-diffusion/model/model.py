6c08ebf860c06d9d02199193dff5feca442137a9
Het Trivedi <het.trivedi05@gmail.com>
2023-11-22 09:26:24 -0800

    Adding more inputs for the model

--- stable-diffusion/stable-video-diffusion/model/model.py
+++ stable-diffusion/stable-video-diffusion/model/model.py
@@ -48,16 +48,25 @@ class Model:
 
     def predict(self, model_input: Dict) -> Dict:
         image_b64 = model_input.get("image")
-        duration = int(model_input.get("duration"))
-        if duration > 10:
-            return {"output": "The duration of the clip cannot be greater than 10"}
+        num_frames = int(model_input.get("num_frames", 14))
+        num_steps = int(model_input.get("num_steps", 25))
+        fps_id = int(model_input.get("fps", 6))
+        frames_decoded_per_second = int(model_input.get("decoding_t", 5))
+
+        if frames_decoded_per_second > 10:
+            return {
+                "output": "GPU does not have enough memory to decode more than 10 frames per second"
+            }
 
         pil_image = b64_to_pil(image_b64)
         pil_image.save("input_image.png")
 
         sample(
             input_path=os.path.join(str(os.getcwd()), "input_image.png"),
-            decoding_t=duration,
+            num_frames=num_frames,
+            num_steps=num_steps,
+            fps_id=fps_id,
+            decoding_t=frames_decoded_per_second,
         )
 
         if os.path.isdir("outputs/simple_video_sample/svd/"):
