470a7fa99d8aeb07925ae5044e94b81d7c8c757c
Timur Abishev <timur.abishev@baseten.co>
2023-11-21 19:11:33 -0500

    generate

--- mistral/mistral-7b-chat/model/model.py
+++ mistral/mistral-7b-chat/model/model.py
@@ -3,18 +3,16 @@ from threading import Thread
 import torch
 from transformers import GenerationConfig, TextIteratorStreamer, pipeline
 
-CHECKPOINT = "mistralai/Mistral-7B-Instruct-v0.1"
-
 
 class Model:
     def __init__(self, **kwargs):
+        self._repo_id = kwargs["config"]["model_metadata"]["model"]
         self._model = None
 
     def load(self):
-
         self._model = pipeline(
             "text-generation",
-            model=CHECKPOINT,
+            model=self._repo_id,
             torch_dtype=torch.bfloat16,
             device_map="auto",
         )
