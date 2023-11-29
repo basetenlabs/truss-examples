16327c2c8391cea970766f20c418dbbc7e02ec8e
Varun Shenoy <vnshenoy@stanford.edu>
2023-11-29 14:35:10 -0800

    Add LCM Dreamshaper Truss (#47)
    This PR adds a Truss for the recently released Dreamshaper Latent
    Consistency Model. Stable Diffusion in ~1s on an A10G.

---
+++ stable-diffusion/dreamshaper-lcm/model/model.py
@@ -0,0 +1,39 @@
+import base64
+from io import BytesIO
+
+import torch
+from diffusers import DiffusionPipeline
+from PIL import Image
+
+
+class Model:
+    def __init__(self, **kwargs):
+        self._model = None
+
+    def load(self):
+        self._model = DiffusionPipeline.from_pretrained(
+            "SimianLuo/LCM_Dreamshaper_v7",
+            custom_pipeline="latent_consistency_txt2img",
+            custom_revision="main",
+        ).to(torch_device="cuda", torch_dtype=torch.float32)
+
+    def convert_to_b64(self, image: Image) -> str:
+        buffered = BytesIO()
+        image.save(buffered, format="JPEG")
+        img_b64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        return img_b64
+
+    def predict(self, model_input):
+        prompt = model_input["prompt"]
+        num_inference_steps = model_input.get("num_inference_steps", 4)
+
+        image = self._model(
+            prompt=prompt,
+            num_inference_steps=num_inference_steps,
+            guidance_scale=8.0,
+            lcm_origin_steps=50,
+            output_type="pil",
+        ).images[0]
+        img_b64 = self.convert_to_b64(image)
+
+        return {"image": img_b64}
