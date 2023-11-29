5e1c3aa266a1769dcabf9f2645984cdf61356ae1
Varun Shenoy <vnshenoy@stanford.edu>
2023-11-29 22:31:38 +0000

    fix whitespace

--- stable-diffusion/dreamshaper-lcm/model/model.py
+++ stable-diffusion/dreamshaper-lcm/model/model.py
@@ -1,15 +1,21 @@
-from diffusers import DiffusionPipeline
-import torch
-from PIL import Image
-from io import BytesIO
 import base64
+from io import BytesIO
+
+import torch
+from diffusers import DiffusionPipeline
+from PIL import Image
+
 
 class Model:
     def __init__(self, **kwargs):
         self._model = None
 
     def load(self):
-        self._model = DiffusionPipeline.from_pretrained("SimianLuo/LCM_Dreamshaper_v7", custom_pipeline="latent_consistency_txt2img", custom_revision="main").to(torch_device="cuda", torch_dtype=torch.float32)
+        self._model = DiffusionPipeline.from_pretrained(
+            "SimianLuo/LCM_Dreamshaper_v7",
+            custom_pipeline="latent_consistency_txt2img",
+            custom_revision="main",
+        ).to(torch_device="cuda", torch_dtype=torch.float32)
 
     def convert_to_b64(self, image: Image) -> str:
         buffered = BytesIO()
@@ -21,7 +27,13 @@ class Model:
         prompt = model_input["prompt"]
         num_inference_steps = model_input.get("num_inference_steps", 4)
 
-        image = self._model(prompt=prompt, num_inference_steps=num_inference_steps, guidance_scale=8.0, lcm_origin_steps=50, output_type="pil").images[0]
+        image = self._model(
+            prompt=prompt,
+            num_inference_steps=num_inference_steps,
+            guidance_scale=8.0,
+            lcm_origin_steps=50,
+            output_type="pil",
+        ).images[0]
         img_b64 = self.convert_to_b64(image)
 
         return {"image": img_b64}
