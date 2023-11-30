8c8fc92ea5a651c5dda407073b06b72dd1f9d907
Het Trivedi <het.trivedi05@gmail.com>
2023-11-30 11:30:57 -0800

    Adding models as external data url

--- stable-diffusion/stable-video-diffusion/model/model.py
+++ stable-diffusion/stable-video-diffusion/model/model.py
@@ -1,7 +1,6 @@
 import os
 from typing import Dict
 
-import requests
 from model.helper import b64_to_pil, mp4_to_base64, sample
 
 original_working_directory = os.getcwd()
@@ -18,38 +17,8 @@ class Model:
         # self._config = kwargs["config"]
         self._model = None
 
-    def download_model(self, model_url, destination_path):
-        print(f"Downloading model {model_url} ...")
-        try:
-            response = requests.get(model_url, stream=True)
-            response.raise_for_status()
-            print("download response: ", response)
-
-            # Open the destination file and write the content in chunks
-            print("opening: ", destination_path)
-            with open(destination_path, "wb") as file:
-                print("writing chunks...")
-                for chunk in response.iter_content(chunk_size=8192):
-                    if chunk:  # Filter out keep-alive new chunks
-                        file.write(chunk)
-
-                print("done writing chunks!!!!")
-
-            print(f"Downloaded file to: {destination_path}")
-        except requests.exceptions.RequestException as e:
-            print(f"Download failed: {e}")
-
     def load(self):
-        os.mkdir("checkpoints")
-        os.mkdir("clip_ckpt")
-        self.download_model(
-            "https://huggingface.co/stabilityai/stable-video-diffusion-img2vid/resolve/main/svd.safetensors?download=true",
-            "checkpoints/svd.safetensors",
-        )
-        self.download_model(
-            "https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt",
-            "clip_ckpt/model.pt",
-        )
+        pass
 
     def predict(self, model_input: Dict) -> Dict:
         image_b64 = model_input.get("image")
