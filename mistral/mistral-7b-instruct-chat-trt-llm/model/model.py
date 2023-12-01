9a0dca5d6d04455850dad87c07709f657a26ca35
Pankaj Gupta <pankaj.roark@gmail.com>
2023-12-01 11:24:00 -0800

    chkpt

--- mistral/mistral-7b-instruct-chat-trt-llm/model/model.py
+++ mistral/mistral-7b-instruct-chat-trt-llm/model/model.py
@@ -6,7 +6,7 @@ import numpy as np
 from client import TritonClient, UserData
 from transformers import AutoTokenizer
 from utils import download_engine, prepare_grpc_tensor
-import uuid
+import random
 
 TRITON_MODEL_REPOSITORY_PATH = Path("/packages/inflight_batcher_llm/")
 
@@ -68,7 +68,8 @@ class Model:
     def predict(self, model_input):
         user_data = UserData()
         model_name = "ensemble"
-        stream_uuid = str(uuid.uuid4())
+        stream_uuid = random.getrandbits(64)
+
 
         if self.uses_openai_api:
             prompt = self.tokenizer.apply_chat_template(
