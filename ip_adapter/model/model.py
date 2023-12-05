255dd441a67a4500668b7d1cc277b126be069727
Varun Shenoy <vnshenoy@stanford.edu>
2023-12-05 09:47:14 -0800

    Add IP Adapter (#109)
    Adds IP adapter to truss examples.

---
+++ ip_adapter/model/model.py
@@ -0,0 +1,52 @@
+import base64
+from io import BytesIO
+
+import torch
+from diffusers import StableDiffusionPipeline
+from PIL import Image
+
+BASE64_PREAMBLE = "data:image/png;base64,"
+
+
+def pil_to_b64(pil_img):
+    buffered = BytesIO()
+    pil_img.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+    return img_str
+
+
+def b64_to_pil(b64_str):
+    return Image.open(BytesIO(base64.b64decode(b64_str.replace(BASE64_PREAMBLE, ""))))
+
+
+class Model:
+    def __init__(self, **kwargs):
+        self.pipeline = None
+
+    def load(self):
+        # Load model here and assign to self._model.
+        self.pipeline = StableDiffusionPipeline.from_pretrained(
+            "runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16
+        )
+        self.pipeline.to("cuda")
+        self.pipeline.load_ip_adapter(
+            "h94/IP-Adapter", subfolder="models", weight_name="ip-adapter_sd15.bin"
+        )
+
+    # more examples here: https://github.com/huggingface/diffusers/pull/5713
+    def predict(self, model_input):
+        input_image = model_input["image"]
+        image = b64_to_pil(input_image)
+
+        # generator = torch.Generator(device="cpu").manual_seed(33)
+        image = self.pipeline(
+            prompt="best quality, high quality",
+            ip_adapter_image=image,
+            negative_prompt="monochrome, lowres, bad anatomy, worst quality, low quality",
+            num_inference_steps=50,
+            # generator=generator,
+        ).images[0]
+
+        output_image = pil_to_b64(image)
+
+        return {"result": output_image}
--- llava/llava-v1.5-7b/README.md
+++ llava/llava-v1.5-7b/README.md
@@ -1,6 +1,6 @@
 # LLaVA v1.5 Truss
 
-This repository packages [LLaVA 1.5](https://github.com/THUDM/CogVLM) as a [Truss](https://truss.baseten.co/).
+This repository packages [LLaVA 1.5](https://github.com/haotian-liu/LLaVA/) as a [Truss](https://truss.baseten.co/).
 
 LLaVA (Large Language and Vision Assistant) is a highly performant open-source vision language model with capabilities similar to GPT-4V.
 
@@ -18,7 +18,7 @@ Before deployment:
 1. Make sure you have a [Baseten account](https://app.baseten.co/signup) and [API key](https://app.baseten.co/settings/account/api_keys).
 2. Install the latest version of Truss: `pip install --upgrade truss`
 
-With `cogvlm` as your working directory, you can deploy the model with:
+With `llava-v1.5-7b` as your working directory, you can deploy the model with:
 
 ```sh
 truss push
