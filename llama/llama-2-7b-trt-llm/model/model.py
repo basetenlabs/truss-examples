2f7e000fe20b45673f25d22bbc44604378c03714
Pankaj Gupta <pankajroark@users.noreply.github.com>
2023-12-06 12:17:19 -0800

    Use randomly generated uuid (#113)
    The id is 32 bit random which is not good in general, but should unblock
    benchmarking with multiple workers. I had difficulties passing longer
    ids to trtllm, so that needs some work. Perhaps we can combine worker id
    with auto-increment id, will explore in follow up.
    
    ---------
    
    Co-authored-by: joostinyi <63941848+joostinyi@users.noreply.github.com>

--- llama/llama-2-7b-trt-llm/model/model.py
+++ llama/llama-2-7b-trt-llm/model/model.py
@@ -1,3 +1,4 @@
+import uuid
 from itertools import count
 from pathlib import Path
 from threading import Thread
@@ -67,7 +68,7 @@ class Model:
     def predict(self, model_input):
         user_data = UserData()
         model_name = "ensemble"
-        stream_uuid = str(next(self._request_id_counter))
+        stream_uuid = str(uuid.uuid4().int & (1 << 32) - 1)
 
         if self.uses_openai_api:
             prompt = self.tokenizer.apply_chat_template(
