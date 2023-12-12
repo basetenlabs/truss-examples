5bb2b867d77bfbee27c313a695c38a50aa2d3dd5
Varun Shenoy <vnshenoy@stanford.edu>
2023-12-12 19:38:04 +0000

    add mixtral

---
+++ mistral/mixtral-8x7b-instruct-vllm/model/model.py
@@ -0,0 +1,34 @@
+import uuid
+import subprocess
+
+from vllm import SamplingParams
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.engine.async_llm_engine import AsyncLLMEngine
+
+class Model:
+    def __init__(self, **kwargs):
+        self.model = None
+        self.llm_engine = None
+
+        # Install megablocks and run ray process
+        subprocess.run(['pip', 'install', 'megablocks'])
+        command = "ray start --head"
+        subprocess.check_output(command, shell=True, text=True)
+
+    def load(self):
+        self.model_args = AsyncEngineArgs(model="mistralai/Mixtral-8x7B-Instruct-v0.1")
+        self.llm_engine = AsyncLLMEngine.from_engine_args(self.model_args)
+
+    async def predict(self, model_input):
+        prompt = model_input.pop("prompt")
+        sampling_params = SamplingParams(**model_input)
+        idx = str(uuid.uuid4().hex)
+        generator = self.llm_engine.generate(prompt, sampling_params, idx)
+
+        # stream output from generator
+        prev = ""
+        async for output in generator:
+            curr = output.outputs[0].text
+            delta = curr[len(prev) :]
+            prev = curr
+            yield delta
