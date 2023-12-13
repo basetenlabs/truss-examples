ce05774007c805061f264b711c3a703093ece9d8
Timur Abishev <timur.abishev@baseten.co>
2023-12-13 16:44:42 -0500

    tmp

--- mistral/mistral-7b-trt-llm-build-engine/model/model.py
+++ mistral/mistral-7b-trt-llm-build-engine/model/model.py
@@ -1,9 +1,9 @@
+import os
 from itertools import count
 from pathlib import Path
 from threading import Thread
 
 import numpy as np
-from build_engine_utils import BuildConfig, build_engine
 from client import TritonClient, UserData
 from transformers import AutoTokenizer
 from utils import download_engine, prepare_grpc_tensor
@@ -18,10 +18,16 @@ class Model:
         self._secrets = kwargs["secrets"]
         self._request_id_counter = count(start=1)
         self.triton_client = None
+        self.tokenizer = None
+        self.uses_openai_api = (
+            "openai-compatible" in self._config["model_metadata"]["tags"]
+        )
 
     def load(self):
-        tensor_parallelism = self._config["model_metadata"].get("tensor_parallelism", 1)
-        pipeline_parallelism = self._config["model_metadata"].get(
+        tensor_parallel_count = self._config["model_metadata"].get(
+            "tensor_parallelism", 1
+        )
+        pipeline_parallel_count = self._config["model_metadata"].get(
             "pipeline_parallelism", 1
         )
         if "hf_access_token" in self._secrets._base_secrets.keys():
@@ -29,13 +35,12 @@ class Model:
         else:
             hf_access_token = None
         is_external_engine_repo = "engine_repository" in self._config["model_metadata"]
-        tokenizer_repository = self._config["model_metadata"]["tokenizer_repository"]
 
         # Instantiate TritonClient
         self.triton_client = TritonClient(
             data_dir=self._data_dir,
             model_repository_dir=TRITON_MODEL_REPOSITORY_PATH,
-            parallel_count=tensor_parallelism * pipeline_parallelism,
+            parallel_count=tensor_parallel_count * pipeline_parallel_count,
         )
 
         # Download model from Hugging Face Hub if specified
@@ -45,24 +50,9 @@ class Model:
                 fp=self._data_dir,
                 auth_token=hf_access_token,
             )
-        if "engine_build" in self._config["model_metadata"]:
-            if not is_external_engine_repo:
-                build_engine(
-                    model_repo=tokenizer_repository,
-                    config=BuildConfig(
-                        **self._config["model_metadata"]["engine_build"]
-                    ),
-                    dst=self._data_dir,
-                    hf_auth_token=hf_access_token,
-                    tensor_parallelism=tensor_parallelism,
-                    pipeline_parallelism=pipeline_parallelism,
-                )
-            else:
-                raise Exception(
-                    "`engine_build` and `engine_repository` can't be specified at the same time"
-                )
 
         # Load Triton Server and model
+        tokenizer_repository = self._config["model_metadata"]["tokenizer_repository"]
         env = {"triton_tokenizer_repository": tokenizer_repository}
         if hf_access_token is not None:
             env["HUGGING_FACE_HUB_TOKEN"] = hf_access_token
@@ -70,17 +60,24 @@ class Model:
         self.triton_client.load_server_and_model(env=env)
 
         # setup eos token
-        tokenizer = AutoTokenizer.from_pretrained(
+        self.tokenizer = AutoTokenizer.from_pretrained(
             tokenizer_repository, token=hf_access_token
         )
-        self.eos_token_id = tokenizer.eos_token_id
+        self.eos_token_id = self.tokenizer.eos_token_id
 
     def predict(self, model_input):
         user_data = UserData()
         model_name = "ensemble"
-        stream_uuid = str(next(self._request_id_counter))
+        stream_uuid = str(os.getpid()) + str(next(self._request_id_counter))
 
-        prompt = model_input.get("prompt")
+        if self.uses_openai_api:
+            prompt = self.tokenizer.apply_chat_template(
+                model_input.get("messages"),
+                tokenize=False,
+            )
+        else:
+            prompt = model_input.get("prompt")
+
         max_tokens = model_input.get("max_tokens", 50)
         beam_width = model_input.get("beam_width", 1)
         bad_words_list = model_input.get("bad_words_list", [""])
@@ -133,4 +130,7 @@ class Model:
         if stream:
             return generate()
         else:
-            return {"text": "".join(generate())}
+            if self.uses_openai_api:
+                return "".join(generate())
+            else:
+                return {"text": "".join(generate())}
