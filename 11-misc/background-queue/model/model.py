ce61beb1b453df374a31d0afb7d2f606ad685474
Bola Malek <bola@baseten.co>
2024-01-12 10:34:00 -0800

    Add polling

--- 11-misc/background-queue/model/model.py
+++ 11-misc/background-queue/model/model.py
@@ -4,10 +4,10 @@ import threading
 import requests
 
 class BackgroundQueueProcessor:
-    def __init__(self, loaded_model, callback_url):
+    def __init__(self, loaded_model):
         self.queue = Queue()
         self.loaded_model = loaded_model
-        self.callback_url = callback_url
+        self.results = {}
 
     def add_to_queue(self, data):
         item_id = uuid.uuid4()
@@ -17,17 +17,21 @@ class BackgroundQueueProcessor:
 
     def process_queue(self):
         while True:
-            print("Waiting for tasks")
             item = self.queue.get()
-            print("got a task")
             # process item here
             item_id = item["id"]
             result = self.loaded_model(item["data"])
             self.queue.task_done()
-            payload = {"result": result, "item_id": item_id}
-            response = requests.post(self.callback_url, json=payload)
-            print(f"POST: {response.status_code}, text: {response.text}")
-
+            self.results[item_id] = result
+    
+    def get_is_done(self, item_id) -> bool:
+        if item_id in self.results:
+            return True
+        return False
+    
+    def get_result(self, item_id):
+        return self.results.pop(item_id)
+            
 import time
 
 class Sleeper:
@@ -36,6 +40,7 @@ class Sleeper:
             print(f"task: {data}, processing {i+1}/6")
             time.sleep(60)  # sleep for 1 minute
         print("Done with data.")
+        return f"result for {data}"
 
 
 class Model:
@@ -48,7 +53,7 @@ class Model:
         # self._secrets = kwargs["secrets"]
         self._model = None
         self._model = Sleeper()
-        self._queue_processor = BackgroundQueueProcessor(self._model, "https://1948-2600-8802-5502-f700-10dd-d39e-b6c-9e36.ngrok-free.app")
+        self._queue_processor = BackgroundQueueProcessor(self._model)
         
         self._thread = threading.Thread(target=self._queue_processor.process_queue)
         self._thread.start()
@@ -58,9 +63,18 @@ class Model:
         # Load model here and assign to self._model.
         pass
 
-    def predict(self, model_input):
-        return {
-            "task_id": self._queue_processor.add_to_queue(model_input)
-        }
+    def predict(self, model_input: dict) -> dict:
+        request_type = model_input.get("type", "queue")
+        if request_type == "queue":
+            return {
+                "task_id": self._queue_processor.add_to_queue(model_input["data"])
+            }
+            
+        if request_type == "status":
+            return {
+                "done": self._queue_processor.get_is_done(model_input["task_id"])
+            }
+        if request_type == "result":
+            return self._queue_processor.get_result(model_input["task_id"])
         
         
