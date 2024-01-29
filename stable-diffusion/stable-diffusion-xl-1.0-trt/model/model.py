75c1309ee6f46d7a531ef7705b255568ebaf4c8a
Pankaj Gupta <pankajroark@users.noreply.github.com>
2024-01-29 11:40:14 -0800

    Use trt engines for clip and clip2 for TensorRT SDXL (#173)
    Clip trt engines have three bindings, "input_ids", "text_embeddings" and
    "hidden_states". These are mapped to the form that diffusers pipeline
    expects; the clip trt engines are thus adapted to replace diffuser
    counterparts in the base and refiner pipelines.
    
    This should restore the 300ms we were missing compared to
    https://huggingface.co/stabilityai/stable-diffusion-xl-1.0-tensorrt

--- stable-diffusion/stable-diffusion-xl-1.0-trt/model/model.py
+++ stable-diffusion/stable-diffusion-xl-1.0-trt/model/model.py
@@ -5,7 +5,9 @@ from typing import Any
 
 import tensorrt as trt
 import torch
+from cuda import cudart
 from diffusers import AutoencoderKL, DiffusionPipeline, DPMSolverMultistepScheduler
+from diffusion.trtclip import TRTClip
 from diffusion.trtunet import TRTUnet
 from huggingface_hub import snapshot_download
 from PIL import Image
@@ -38,20 +40,31 @@ class Model:
         # DPM++ 2M SDE Karras (for 30+ steps, when speed doesn't matter)
         # self.pipe.scheduler = DPMSolverMultistepScheduler.from_config(self.pipe.scheduler.config, algorithm_type="sde-dpmsolver++", use_karras_sigmas=True)
 
+        cuda_stream = cudart.cudaStreamCreate()[1]
         self.pipe.unet.to(memory_format=torch.channels_last)
         self.pipe.to("cuda")
-        self.pipe.unet = _wrap_in_tunet(self.pipe.unet, "engine")
+        self.pipe.unet = _wrap_in_tunet(self.pipe.unet, "engine", cuda_stream)
+        self.pipe.text_encoder = _wrap_in_trtclip(
+            self.pipe.text_encoder, "engine", cuda_stream
+        )
+        self.pipe.text_encoder_2 = _wrap_in_trtclip2(
+            self.pipe.text_encoder_2, "engine", cuda_stream
+        )
 
         self.refiner = DiffusionPipeline.from_pretrained(
             "stabilityai/stable-diffusion-xl-refiner-1.0",
-            text_encoder_2=self.pipe.text_encoder_2,
             vae=self.pipe.vae,
             torch_dtype=torch.float16,
             use_safetensors=True,
             variant="fp16",
         )
         self.refiner.to("cuda")
-        self.refiner.unet = _wrap_in_tunet(self.refiner.unet, "engine_xl_refiner")
+        self.refiner.text_encoder_2 = _wrap_in_trtclip2(
+            self.refiner.text_encoder_2, "engine_xl_refiner", cuda_stream
+        )
+        self.refiner.unet = _wrap_in_tunet(
+            self.refiner.unet, "engine_xl_refiner", cuda_stream
+        )
 
         # image = self.pipe(prompt="a golden retriever", num_inference_steps=30, output_type="pil").images[0]
         # image = self.refiner(prompt="a golden retriever", num_inference_steps=30, output_type="pil").images[0]
@@ -135,12 +148,26 @@ class Model:
         return {"status": "success", "data": b64_results, "time": end_time}
 
 
-def _wrap_in_tunet(unet, engine_dir_name):
-    unet.to("cpu")
+def _wrap_in_tunet(unet, engine_dir_name, stream):
+    return _wrap_in_trt(unet, engine_dir_name, "unetxl", TRTUnet, stream)
+
+
+def _wrap_in_trtclip(clip, engine_dir_name, stream):
+    return _wrap_in_trt(clip, engine_dir_name, "clip", TRTClip, stream, is_clip2=False)
+
+
+def _wrap_in_trtclip2(clip, engine_dir_name, stream):
+    return _wrap_in_trt(clip, engine_dir_name, "clip2", TRTClip, stream, is_clip2=True)
+
+
+def _wrap_in_trt(model, engine_dir_name, engine_name, trt_class, stream, **kwargs):
+    model.to("cpu")
     torch.cuda.empty_cache()
-    tunet = TRTUnet(
-        unet,
-        engine_path=f"/app/data/{engine_dir_name}/unetxl.trt{trt.__version__}.plan",
+    trt_model = trt_class(
+        model,
+        stream=stream,
+        engine_path=f"/app/data/{engine_dir_name}/{engine_name}.trt{trt.__version__}.plan",
+        **kwargs,
     )
-    tunet.load()
-    return tunet
+    trt_model.load()
+    return trt_model
