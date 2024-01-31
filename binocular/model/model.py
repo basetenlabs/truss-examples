c5ca31711c8055bc615909db4f4d2bbfa5643a66
Bola Malek <bola@baseten.co>
2024-01-31 14:17:20 -0800

    Fix lint

--- binocular/model/model.py
+++ binocular/model/model.py
@@ -1,5 +1,6 @@
-from binoculars import Binoculars
 import logging
+
+from binoculars import Binoculars
 
 MINIMUM_TOKENS = 64
 
@@ -12,18 +13,16 @@ class Model:
         # Load model here and assign to self._model.
         self._model = Binoculars()
         self._tokenizer = self._model.tokenizer
-    
-    
+
     def count_tokens(self, text):
         return len(self._tokenizer(text).input_ids)
-
 
     def predict(self, model_input: dict):
         input_text = model_input.pop("text")
         if self.count_tokens(input_text) < MINIMUM_TOKENS:
             logging.warn("Insufficient content length")
             return {}
-            
+
         return {
             "score": self._model.compute_score(input_text),
             "label": self._model.predict(input_text),
