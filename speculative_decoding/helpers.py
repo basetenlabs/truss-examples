5143dc94678b44af656ecbd748c4e1e8edc5f94f
marius.baseten <marius.killinger@baseten.co>
2024-02-05 19:45:03 +0000

    More granular timing. Organize helpers

---
+++ speculative_decoding/helpers.py
@@ -0,0 +1,135 @@
+import collections
+import contextlib
+import time
+from typing import NamedTuple, Union
+
+import numpy as np
+import pandas as pd
+import tritonclient.grpc as triton_grpc
+import tritonclient.utils as triton_utils
+
+
+class SectionTiming(NamedTuple):
+    name: str
+    count: str = 0
+    total_time: float = 0
+
+
+class _Timing(object):
+    """Accumulator that collects statistics (esp. execution time) over repeated updates and offers pretty printing."""
+
+    def __init__(self):
+        self._values = collections.deque(maxlen=50)
+        self._count = 0
+        self._sum = 0
+        self._t0 = 0
+
+    def update(self, value: Union[float, int]):
+        self._values.append(value)
+        self._count += 1
+        self._sum += value
+
+    def reset(self):
+        self.__init__()
+
+    def start_time(self):
+        assert self._t0 == 0
+        self._t0 = time.time()
+
+    def record_time(self):
+        t = time.time() - self._t0
+        self.update(t)
+        self._t0 = 0
+
+    def __str__(self) -> str:
+        vals = np.array(self._values)
+        avg = self._sum / self._count
+        its = 1 / avg
+        return (
+            f"Σ={self._sum:.3g} ({its:.3g}/s)\tμ={avg:.3g}\tmin={vals.min():.3g}\t"
+            f"max={vals.max():.3g}\tσ/μ={vals.var() / avg:.3g}\tN={self._count}"
+        )
+
+    def to_dict(self) -> dict[str, float | int | str]:
+        vals = np.array(self._values)
+        avg = self._sum / self._count
+        its = 1 / avg
+        return {
+            "Σ": self._sum,
+            "it/s": its,
+            "μ": avg,
+            "N": self._count,
+            "min": vals.min(),
+            "max": vals.max(),
+        }
+
+
+class _TimingManager(object):
+    """Container to manage a set of global metrics for execution times."""
+
+    def __init__(self):
+        self._timings = collections.defaultdict(_Timing)
+        self._order = []
+        self._total = None
+
+    def start_time(self, name: str):
+        if name not in self._timings:
+            self._order.append(name)
+        self._timings[name].start_time()
+
+    def record_time(self, name: str):
+        self._timings[name].record_time()
+
+    def __str__(self):
+        parts = ["Global Wall Time Totals:"]
+        for n in self._order:
+            m = self._timings[n]
+            if self._total:
+                t = self._timings[self._total].sum
+                fraction = f"    {m.sum / t * 100:2.0f}% "
+            else:
+                fraction = ""
+
+            parts.append(f"{n:<20}: {fraction}{m}")
+        return "\n".join(parts)
+
+    def to_pd(self) -> pd.DataFrame:
+        rows = []
+        section_key = "section"
+        columns = [section_key, "Σ", "μ", "N", "it/s", "min", "max"]
+        for n in self._order:
+            t_dict = self._timings[n].to_dict()
+            t_dict[section_key] = n
+            rows.append(t_dict)
+        return pd.DataFrame(data=rows, columns=columns).set_index(section_key)
+
+    def reset(self):
+        self.__init__()
+
+    def set_total(self, name: str):
+        self._total = name
+
+
+_global_metrics = _TimingManager()
+
+
+@contextlib.contextmanager
+def timeit(name: str):
+    try:
+        _global_metrics.start_time(name)
+        yield
+    finally:
+        _global_metrics.record_time(name)
+
+
+def show_timings():
+    print(_global_metrics.to_pd())
+
+
+def append_tensor(name: str, input_data, dtype: np.dtype, mutable_inputs: list) -> None:
+    array_input = np.asarray(input_data, dtype=dtype)
+    t = triton_grpc.InferInput(
+        name, array_input.shape, triton_utils.np_to_triton_dtype(dtype)
+    )
+    t.set_data_from_numpy(array_input)
+    mutable_inputs.append(t)
--- speculative_decoding/run_spec_dec.py
+++ speculative_decoding/run_spec_dec.py
@@ -1,57 +1,7 @@
-import collections
-import time
-from typing import MutableMapping, NamedTuple
-
+import helpers  # TODO: this dep is relative and not portable
 import numpy as np
 import transformers
-import tritonclient.grpc as grpcclient
-from tritonclient.utils import np_to_triton_dtype
-
-
-class SectionTiming(NamedTuple):
-    name: str
-    count: str = 0
-    total_time: float = 0
-
-
-_SECTION_TIMINGS: MutableMapping[str, SectionTiming] = collections.defaultdict(
-    SectionTiming
-)
-
-
-def _show_timings():
-    for section, timing in _SECTION_TIMINGS.items():
-        print(f"{section}: {timing.count:02} x total {timing.total_time} sec.")
-
-
-class _TimerContextManager:
-    def __init__(self, section_name: str) -> None:
-        self._section_name = section_name
-
-    def __enter__(self):
-        self.start_time = time.time()
-        return self
-
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        end_time = time.time()
-        elapsed_time = end_time - self.start_time
-        timing = _SECTION_TIMINGS[self._section_name]
-        timing.name = self._section_name
-        timing.count += 1
-        timing.total_time += elapsed_time
-        # print(f"Run `{self._section_name}` in `{elapsed_time}` seconds.")
-
-
-def timeit(section_name: str) -> _TimerContextManager:
-    return _TimerContextManager(section_name)
-
-
-def _append_tensor(name: str, input, dtype: np.dtype, mutable_inputs: list) -> None:
-    if input is not None:
-        array_input = np.asarray(input, dtype=dtype)
-        t = grpcclient.InferInput(name, array_input.shape, np_to_triton_dtype(dtype))
-        t.set_data_from_numpy(array_input)
-        mutable_inputs.append(t)
+import tritonclient.grpc as triton_grpc
 
 
 def _make_trtllm_inputs(
@@ -71,25 +21,40 @@ def _make_trtllm_inputs(
 ):
     inputs = []
     # Add batch dimension.
-    _append_tensor("input_ids", np.expand_dims(input_ids, axis=0), np.int32, inputs)
-    _append_tensor("input_lengths", [[input_length]], np.int32, inputs)
-    _append_tensor("request_output_len", [[num_gen_tokens]], np.uint32, inputs)
-    _append_tensor("draft_input_ids", [draft_tokens], np.int32, inputs)
+    helpers.append_tensor(
+        "input_ids", np.expand_dims(input_ids, axis=0), np.int32, inputs
+    )
+    helpers.append_tensor("input_lengths", [[input_length]], np.int32, inputs)
+    helpers.append_tensor("request_output_len", [[num_gen_tokens]], np.uint32, inputs)
+    helpers.append_tensor("draft_input_ids", [draft_tokens], np.int32, inputs)
     # Optional inputs
-    _append_tensor("return_log_probs", [[True]], bool, inputs)
-    _append_tensor("beam_width", [[beam_width]], np.uint32, inputs)
-    _append_tensor("temperature", [[temperature]], np.float32, inputs)
+    helpers.append_tensor("return_log_probs", [[True]], bool, inputs)
+    if beam_width is not None:
+        helpers.append_tensor("beam_width", [[beam_width]], np.uint32, inputs)
+    if temperature is not None:
+        helpers.append_tensor("temperature", [[temperature]], np.float32, inputs)
     # The return_X_logits tensor names were only added in
     # https://github.com/NVIDIA/TensorRT-LLM/pull/846.
     # prepare_tensor("return_context_logits", np.array([[True]], dtype=bool)),
     # prepare_tensor("return_generation_logits", np.array([[True]], dtype=bool)),
     # prepare_tensor("bad_words_list", bad_words_ids),
     # prepare_tensor("stop_words_list", stop_words_ids),
-    _append_tensor("repetition_penalty", [[repetition_penalty]], np.float32, inputs)
-    _append_tensor("presence_penalty", [[presence_penalty]], np.float32, inputs)
-    _append_tensor("frequency_penalty", [[frequency_penalty]], np.float32, inputs)
-    _append_tensor("end_id", [[end_id]], np.uint32, inputs)
-    _append_tensor("pad_id", [[pad_id]], np.uint32, inputs)
+    if repetition_penalty is not None:
+        helpers.append_tensor(
+            "repetition_penalty", [[repetition_penalty]], np.float32, inputs
+        )
+    if presence_penalty is not None:
+        helpers.append_tensor(
+            "presence_penalty", [[presence_penalty]], np.float32, inputs
+        )
+    if frequency_penalty is not None:
+        helpers.append_tensor(
+            "frequency_penalty", [[frequency_penalty]], np.float32, inputs
+        )
+    if end_id is not None:
+        helpers.append_tensor("end_id", [[end_id]], np.uint32, inputs)
+    if pad_id is not None:
+        helpers.append_tensor("pad_id", [[pad_id]], np.uint32, inputs)
     return inputs
 
 
@@ -119,19 +84,19 @@ def run_speculative_inference(
     bad_words,
     beam_width,
     draft_model_name,
+    draft_tokenizer,
     target_model_name,
+    target_tokenizer,
     verbose,
 ):
-    draft_tokenizer = transformers.AutoTokenizer.from_pretrained("gpt2")
     # TODO: check representation of word lists as tokens.
     # draft_bad_words_ids = draft_tokenizer.encode(bad_words)
     # draft_stop_words_ids = draft_tokenizer.encode(stop_words)
-    target_tokenizer = transformers.AutoTokenizer.from_pretrained(
-        "mistralai/Mistral-7B-v0.1"
-    )
 
     def call_draft_model(input_text: str) -> str:
-        input_ids = draft_tokenizer.encode(input_text)
+        with helpers.timeit("draft_encode"):
+            input_ids = draft_tokenizer.encode(input_text)
+
         num_draft_tokens = min(
             max_num_draft_tokens, request_output_len - len(input_ids)
         )
@@ -148,25 +113,30 @@ def run_speculative_inference(
                 f"\n\t{input_ids}\n\t`{input_text}`"
             )
 
-        inputs = _make_trtllm_inputs(
-            input_ids,
-            len(input_ids),
-            num_draft_tokens,
-            None,  # draft_tokens.
-            beam_width,
-            temperature,
-            repetition_penalty,
-            presence_penalty,
-            frequency_penalty,
-            # draft_bad_words_ids,
-            # draft_stop_words_ids,
-            draft_tokenizer.eos_token_id,
-            draft_tokenizer.pad_token_id,
-        )
-        result = client.infer(draft_model_name, inputs, request_id=request_id)
+        with helpers.timeit("draft_input_prep"):
+            inputs = _make_trtllm_inputs(
+                input_ids,
+                len(input_ids),
+                num_draft_tokens,
+                [],  # draft_tokens.
+                beam_width,
+                temperature,
+                repetition_penalty,
+                presence_penalty,
+                frequency_penalty,
+                # draft_bad_words_ids,
+                # draft_stop_words_ids,
+                draft_tokenizer.eos_token_id,
+                draft_tokenizer.pad_token_id,
+            )
+        with helpers.timeit("draft_generate"):
+            result = client.infer(draft_model_name, inputs, request_id=request_id)
+
         output_ids = _extract_trtllm_outputs(result)
 
-        clear_text_full = draft_tokenizer.decode(output_ids)
+        with helpers.timeit("draft_decode"):
+            clear_text_full = draft_tokenizer.decode(output_ids)
+
         if verbose:
             draft_ids = output_ids[len(input_ids) :]
             draft_text = clear_text_full[len(input_text) :]
@@ -177,7 +147,8 @@ def run_speculative_inference(
         return clear_text_full
 
     def call_target_model(input_text: str, confirmed_ids: str) -> tuple[str, list[int]]:
-        input_ids = target_tokenizer.encode(input_text)
+        with helpers.timeit("target_encode"):
+            input_ids = target_tokenizer.encode(input_text)
         draft_ids = input_ids[
             len(confirmed_ids) : len(confirmed_ids) + max_num_draft_tokens
         ]
@@ -200,25 +171,30 @@ def run_speculative_inference(
             )
 
         num_gen_tokens = len(draft_ids) + 1 if draft_ids else 1
-        inputs = _make_trtllm_inputs(
-            confirmed_ids_re_encoded,
-            len(confirmed_ids_re_encoded),
-            num_gen_tokens,
-            draft_ids or None,
-            beam_width,
-            temperature,
-            repetition_penalty,
-            presence_penalty,
-            frequency_penalty,
-            # target_bad_words_ids,
-            # target_stop_words_ids,
-            target_tokenizer.eos_token_id,
-            target_tokenizer.pad_token_id,
-        )
-        result = client.infer(target_model_name, inputs, request_id=request_id)
-        output_ids = _extract_trtllm_outputs(result)
+        with helpers.timeit("target_input_prep"):
+            inputs = _make_trtllm_inputs(
+                confirmed_ids_re_encoded,
+                len(confirmed_ids_re_encoded),
+                num_gen_tokens,
+                draft_ids or None,
+                beam_width,
+                temperature,
+                repetition_penalty,
+                presence_penalty,
+                frequency_penalty,
+                # target_bad_words_ids,
+                # target_stop_words_ids,
+                target_tokenizer.eos_token_id,
+                target_tokenizer.pad_token_id,
+            )
+        with helpers.timeit("target_verify"):
+            result = client.infer(target_model_name, inputs, request_id=request_id)
 
-        clear_text_full = target_tokenizer.decode(output_ids, skip_special_tokens=True)
+        output_ids = _extract_trtllm_outputs(result)
+        with helpers.timeit("target_decode"):
+            clear_text_full = target_tokenizer.decode(
+                output_ids, skip_special_tokens=True
+            )
         if verbose:
             print(
                 f"  Result TARGET model:\n\t{output_ids}\n"
@@ -232,7 +208,7 @@ def run_speculative_inference(
             input_ids,
             len(input_ids),
             num_gen_tokens - len(input_ids),
-            None,
+            [],
             beam_width,
             temperature,
             repetition_penalty,
@@ -243,7 +219,9 @@ def run_speculative_inference(
             target_tokenizer.eos_token_id,
             target_tokenizer.pad_token_id,
         )
-        result = client.infer(target_model_name, inputs, request_id=request_id)
+        with helpers.timeit("target_generate"):
+            result = client.infer(target_model_name, inputs, request_id=request_id)
+
         output_ids = _extract_trtllm_outputs(result)
         clear_text_full = target_tokenizer.decode(output_ids, skip_special_tokens=True)
         if verbose:
@@ -253,22 +231,24 @@ def run_speculative_inference(
             )
         return clear_text_full
 
-    # Warmup model with unrelated string.
-    generate_target_model("What is a computer", request_output_len)
-    call_draft_model("What is a computer")
+    ####################################################################################
+
+    # Warmup models with unrelated string.
+    generate_target_model("What is a computer?", request_output_len)
+    call_draft_model("What is a computer?")
 
     # Run `direct_gen` in `0.26770877838134766` seconds.
-    # with timeit("direct_gen"):
+    # with helpers.timeit("direct_gen"):
     #     generate_target_model(prompt, request_output_len)
 
     # Run `speculative_gen` in `0.1786513328552246` seconds.
-    with timeit("speculative_gen"):
+    with helpers.timeit("speculative_gen"):
         confirmed_target_ids = target_tokenizer.encode(prompt)
         current_text = prompt
         while True:
-            with timeit("draft"):
+            with helpers.timeit("draft_total"):
                 current_text = call_draft_model(current_text)
-            with timeit("target"):
+            with helpers.timeit("target_total"):
                 current_text, confirmed_target_ids = call_target_model(
                     current_text, confirmed_target_ids
                 )
@@ -278,21 +258,25 @@ def run_speculative_inference(
             if len(confirmed_target_ids) == request_output_len:
                 break
 
-    with timeit("direct_gen"):
+    with helpers.timeit("direct_gen"):
         print(generate_target_model(prompt, request_output_len))
 
-    _show_timings
+    helpers.show_timings()
 
     return current_text
 
 
 if __name__ == "__main__":
-    client_ = grpcclient.InferenceServerClient("0.0.0.0:8001")
+    client_ = triton_grpc.InferenceServerClient("0.0.0.0:8001")
+    draft_tokenizer_ = transformers.AutoTokenizer.from_pretrained("gpt2")
+    target_tokenizer_ = transformers.AutoTokenizer.from_pretrained(
+        "mistralai/Mistral-7B-v0.1"
+    )
 
     output_text = run_speculative_inference(
         client_,
         prompt="Once upon a time there was",
-        request_output_len=25,
+        request_output_len=80,
         max_num_draft_tokens=4,
         request_id="1",
         repetition_penalty=None,
@@ -303,7 +287,9 @@ if __name__ == "__main__":
         bad_words=[],
         beam_width=1,
         draft_model_name="draft_model",
+        draft_tokenizer=draft_tokenizer_,
         target_model_name="target_model",
+        target_tokenizer=target_tokenizer_,
         verbose=True,
     )
 
