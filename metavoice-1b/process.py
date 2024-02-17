c28ff49ee28af5811a787e9bd703964586b533bf
strangerstork <vlad@stork.network>
2024-02-17 00:05:33 -0500

    clarifying purpose of split

--- metavoice-1b/process.py
+++ metavoice-1b/process.py
@@ -3,7 +3,7 @@ import sys
 
 b64_audio = sys.stdin.read()
 
-# sometimes the b64 data is surrounded by weird stuff and quotes; let's grab everything within the quotes
+# b64 data is surrounded by info messages and quotes if piped in from a truss command
 b64_audio = b64_audio.split('"')[1]
 
 wav_file = open("output.wav", "wb")
