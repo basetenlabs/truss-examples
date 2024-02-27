d37321641edf3b367d7a710b76ce949899fb8257
Het Trivedi <het.trivedi05@gmail.com>
2024-02-26 22:20:08 -0800

    Updating whisper interface

--- whisper/faster-whisper-v2/model/model.py
+++ whisper/faster-whisper-v2/model/model.py
@@ -22,13 +22,11 @@ class Model:
 
         if audio_base64 and url:
             return {
-                "status": 400,
-                "msg": "Only a base64 audio file OR a URL can be passed to the API, not both of them!",
+                "error": "Only a base64 audio file OR a URL can be passed to the API, not both of them.",
             }
         if not audio_base64 and not url:
             return {
-                "status": 400,
-                "msg": "Please provide either an audio file in base64 string format or a URL to an audio file.",
+                "error": "Please provide either an audio file in base64 string format or a URL to an audio file.",
             }
 
         binary_data = None
@@ -39,10 +37,10 @@ class Model:
             resp = requests.get(url)
             binary_data = resp.content
 
-        return {"status": 200, "data": binary_data}
+        return {"data": binary_data}
 
     def predict(self, request: Dict) -> Dict:
-        if request.get("status") != 200:
+        if request.get("error"):
             return request
 
         audio_data = request.get("data")
