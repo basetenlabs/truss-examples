5e01087a81370d25e2b00b5f432639d8fd687452
Het Trivedi <het.trivedi05@gmail.com>
2024-03-04 13:53:53 -0800

    Fixing issue with torchserve not being available on startup (#229)

--- whisper/whisper-torchserve/model/model.py
+++ whisper/whisper-torchserve/model/model.py
@@ -9,12 +9,14 @@ import requests
 from huggingface_hub import snapshot_download
 
 TORCHSERVE_ENDPOINT = "http://0.0.0.0:8888/predictions/whisper_base"
+TORCHSERVE_HEALTH_ENDPOINT = "http://0.0.0.0:8888/ping"
 
 
 class Model:
     def __init__(self, **kwargs):
         self._data_dir = kwargs["data_dir"]
         self._model = None
+        self.torchserver_ready = False
 
     def start_tochserver(self):
         subprocess.run(
@@ -39,10 +41,21 @@ class Model:
             local_dir=os.path.join(self._data_dir, "model_store"),
             max_workers=4,
         )
-        print("Downloaded weights succesfully!")
+        logging.info("⚡️ Weights Downloaded Successfully!")
 
         process = multiprocessing.Process(target=self.start_tochserver)
         process.start()
+
+        # Need to wait for the torchserve server to start up
+        while not self.torchserver_ready:
+            try:
+                res = requests.get(TORCHSERVE_HEALTH_ENDPOINT)
+                if res.status_code == 200:
+                    self.torchserver_ready = True
+                    logging.info("🔥Torchserve is ready!")
+            except Exception as e:
+                logging.info("⏳Torchserve is loading...")
+                time.sleep(5)
 
     async def predict(self, request: Dict):
         audio_base64 = request.get("audio")
@@ -50,7 +63,7 @@ class Model:
 
         async with httpx.AsyncClient() as client:
             res = await client.post(
-                TORCHSERVE_ENDPOINT, files={"data": (None, audio_bytes)}
+                TORCHSERVE_ENDPOINT, files={"data": (None, audio_bytes)}, timeout=120
             )
             transcription = res.text
         return {"output": transcription}
