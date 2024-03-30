423f447621d5b52b1c67094d065a2269b472b57c
Affan Hussain <affan98@gmail.com>
2024-03-29 17:21:54 -0700

    fix precommit

--- bin/test_truss_deploy.py
+++ bin/test_truss_deploy.py
@@ -1,9 +1,11 @@
-import yaml
-import subprocess
-import re
 import os
-import requests
+import re
+import subprocess
 import time
+from datetime import datetime
+
+import requests
+import yaml
 
 API_KEY = os.environ["BASETEN_API_KEY"]
 
@@ -13,6 +15,7 @@ def get_model_dir():
         ["git", "diff", "--name-only", "origin/main", "HEAD"], capture_output=True
     )
     changed_files = result.stdout.decode().split("\n")
+    print(changed_files)
     for file in changed_files:
         if re.match(r".*/model/model\.py", file):
             return os.path.dirname(os.path.dirname(file))
@@ -53,22 +56,27 @@ remote_url = https://app.baseten.co"""
 
     result = subprocess.run(["truss", "push", "--trusted"], capture_output=True)
     match = re.search(
-        r"View logs for your deployment at \n?https://app\.baseten\.co/models/(\w+)/logs/\w+",
+        r"View logs for your deployment at \n?https://app\.baseten\.co/models/(\w+)/logs/(\w+)",
         result.stdout.decode(),
     )
     if not match:
         raise Exception(
             f"Failed to push model:\n\nSTDOUT: {result.stdout.decode()}\nSTDERR: {result.stderr.decode()}"
         )
-    print(f"Model pushed successfully. model-id: {match.group(1)}")
-    return match.group(1)
+    model_id = match.group(1)
+    deployment_id = match.group(2)
+    print(
+        f"Model pushed successfully. model-id: {model_id}. deployment-id: {deployment_id}"
+    )
+    return model_id, deployment_id
 
 
 def truss_predict(model_id, input):
-    result = "Model is not ready, it is still building or deploying"
-    seconds_remaining = 60 * 15  # Wait for 15 minutes
+    result = {"error": "Model is not ready, it is still building or deploying"}
+    seconds_remaining = 60 * 30  # Wait for 30 minutes
     while (
-        result == "Model is not ready, it is still building or deploying"
+        "error" in result
+        and result["error"] == "Model is not ready, it is still building or deploying"
         and seconds_remaining > 0
     ):
         print(f"{round(seconds_remaining / 60, 2)} minutes remaining")
@@ -77,9 +85,16 @@ def truss_predict(model_id, input):
             headers={"Authorization": f"Api-Key {API_KEY}"},
             json=input,
         )
-        output = result.json()
-        if "error" in output:
-            result = output["error"]
+
+        try:
+            result = result.json()
+        except requests.exceptions.JSONDecodeError as e:
+            print(f"Failed to decode JSON: {e}")
+            print(result.text)
+            return
+
+        if not isinstance(result, dict):
+            return result
 
         seconds_remaining -= 30
         print("Waiting for model to be ready...")
@@ -88,24 +103,23 @@ def truss_predict(model_id, input):
     return result
 
 
-# def get_truss_logs(model_id, start_time):
-#     baseten_api_key = os.environ["BASETEN_API_KEY"]
-#     result = requests.post(
-#         "https://app.baseten.co/logs",
-#         headers={"Authorization": f"Api-Key {baseten_api_key}"},
-#         json={
-#             "type": "MODEL",
-#             "start": 1711145005117,
-#             "end": 1711145305117,
-#             "levels": [],
-#             "regex": "",
-#             "limit": 500,
-#             "entity_id": "e3mzx4zw",
-#             "direction": "backward",
-#         },
-#     )
-#     # {'success': False, 'message': 'Failed to load logs'}
-#     return result
+def get_truss_logs(deployment_id, start_time):
+    result = requests.post(
+        "https://app.baseten.co/logs",
+        headers={"Authorization": f"Api-Key {API_KEY}"},
+        json={
+            "type": "MODEL",
+            "start": start_time,
+            "end": get_time_in_ms(),
+            "levels": [],
+            "regex": "",
+            "limit": 500,
+            "entity_id": deployment_id,
+            "direction": "backward",
+        },
+    )
+    # {'success': False, 'message': 'Failed to load logs'}
+    return result.json()
 
 
 def deactivate_truss(model_id):
@@ -118,8 +132,14 @@ def deactivate_truss(model_id):
     print(result)
 
 
-# def get_time_in_ms():
-#     time.time_ns() // 1_000_000
+def print_formatted_logs(logs):
+    for log in reversed(logs):
+        ts = datetime.fromtimestamp(int(log["ts"]) // 1_000_000_000).ctime()
+        print(f"{ts} - {log['level']} - {log['msg']}")
+
+
+def get_time_in_ms():
+    return time.time_ns() // 1_000_000
 
 
 if __name__ == "__main__":
@@ -129,11 +149,19 @@ if __name__ == "__main__":
 
     os.chdir(model_dir)
     example_input = get_example_input(image_str)
-    model_id = truss_push()
-    deactivate_truss(model_id)
+    model_id, deployment_id = truss_push()
+    start_time = get_time_in_ms()
+    result = truss_predict(model_id, example_input)
+    print(f"Model prediction result: {result}")
+    if "error" in result:
+        logs = get_truss_logs(deployment_id, start_time)
+        if logs["success"]:
+            print_formatted_logs(logs["logs"])
+            print(
+                f"Failed to make prediction. Received error from model {result['error']}"
+            )
+            exit(1)
+        else:
+            print("Failed to load logs")
 
-    # TODO
-    # result = truss_predict(model_id, example_input)
-    # if "error" in result:
-    #     logs = get_truss_logs(model_id, start_time)
-    #     print(logs)
+    deactivate_truss(model_id)
