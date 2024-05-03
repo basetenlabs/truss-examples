f9186e0688283f524b7199967ac7f16cebb1aaf6
Philip Kiely <philip.kiely@baseten.co>
2024-05-02 20:33:25 -0500

    fix issue with nomic model

--- nomic/nomic-embed-v1_5/model/model.py
+++ nomic/nomic-embed-v1_5/model/model.py
@@ -1,10 +1,12 @@
 import torch.nn.functional as F
+from huggingface_hub import login as hf_login
 from sentence_transformers import SentenceTransformer
 
 
 class Model:
     def __init__(self, **kwargs):
         self._model = None
+        self._hf_access_token = kwargs["secrets"]["hf_access_token"]
 
     def preprocess(self, model_input):
         task_type = model_input.get("task_type", "search_document")
@@ -14,6 +16,7 @@ class Model:
         return model_input
 
     def load(self):
+        hf_login(token=self._hf_access_token)
         self._model = SentenceTransformer(
             "nomic-ai/nomic-embed-text-v1.5", trust_remote_code=True
         )
