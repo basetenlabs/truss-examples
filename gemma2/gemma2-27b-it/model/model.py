cb23cd032b66c6380060c8b7d81c801b8f4f25c8
Dhruv Singal <dhruvsingalabc@gmail.com>
2024-07-01 18:59:22 -0700

    fixed formatting

--- gemma2/gemma2-27b-it/model/model.py
+++ gemma2/gemma2-27b-it/model/model.py
@@ -10,6 +10,8 @@ from transformers import (
 )
 
 CHECKPOINT = "google/gemma-2-27b-it"
+
+
 class Model:
     def __init__(self, **kwargs) -> None:
         self.tokenizer = None
@@ -18,28 +20,39 @@ class Model:
             self._secrets = kwargs["secrets"]
         else:
             raise ValueError("Missing secrets")
+
     def load(self):
         # make sure token exists
         if not self._secrets["hf_access_token"]:
             raise ValueError("Missing hf_access_token")
         # huggingface auth is done in the local-gemma script, so we just need to call it
-        command = ["local-gemma", "--model", "27b", "--token", self._secrets["hf_access_token"], "What is the capital of France?"]
+        command = [
+            "local-gemma",
+            "--model",
+            "27b",
+            "--token",
+            self._secrets["hf_access_token"],
+            "What is the capital of France?",
+        ]
 
         try:
             result = subprocess.run(command, capture_output=True, text=True, check=True)
             print(result.stdout)
         except subprocess.CalledProcessError as e:
             print(f"Command failed with code {e.returncode}: {e.stderr}")
-        self.tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT, token=self._secrets["hf_access_token"])
-        self.model = LocalGemma2ForCausalLM.from_pretrained(CHECKPOINT, preset="auto", token=self._secrets["hf_access_token"])
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            CHECKPOINT, token=self._secrets["hf_access_token"]
+        )
+        self.model = LocalGemma2ForCausalLM.from_pretrained(
+            CHECKPOINT, preset="auto", token=self._secrets["hf_access_token"]
+        )
 
     def predict(self, request: Dict) -> Dict:
         prompt = request.pop("prompt")
         # Instantiate the Streamer object, which we'll later use for
         # returning the output to users.
         streamer = TextIteratorStreamer(self.tokenizer)
-        messages = [
-        {"role": "user", "content": prompt}]
+        messages = [{"role": "user", "content": prompt}]
         encodeds = self.tokenizer.apply_chat_template(messages, return_tensors="pt")
         model_inputs = encodeds.to(self.model.device)
         # When creating the generation parameters, ensure to pass the `streamer` object
