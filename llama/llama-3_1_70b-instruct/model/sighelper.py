7b29f7521bc39b65dfecb6abea34919fa5792409
Het Trivedi <het.trivedi05@gmail.com>
2024-07-23 12:04:31 -0700

    Adding llama 3.1 70B vllm

---
+++ llama/llama-3_1_70b-instruct/model/sighelper.py
@@ -0,0 +1,26 @@
+import fileinput
+import sys
+
+MODULE_FILE_PATH = (
+    "/usr/local/lib/python3.10/dist-packages/vllm/executor/multiproc_gpu_executor.py"
+)
+
+
+def patch():
+    # This is for SIGINT
+    search_text = "signal.signal(signal.SIGINT, shutdown)"
+
+    with fileinput.FileInput(MODULE_FILE_PATH, inplace=True) as file:
+        for line in file:
+            if search_text in line:
+                line = "    # " + line.lstrip()
+            sys.stdout.write(line)
+
+    # This is for SIGTERM
+    search_text = "signal.signal(signal.SIGTERM, shutdown)"
+
+    with fileinput.FileInput(MODULE_FILE_PATH, inplace=True) as file:
+        for line in file:
+            if search_text in line:
+                line = "    # " + line.lstrip()
+            sys.stdout.write(line)
