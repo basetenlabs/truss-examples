eee2df6d47f1a948b6672bea83af541dc1993b83
Tianshu Cheng <tianshuc@Tianshus-MacBook-Pro.local>
2024-08-07 11:56:41 -0700

    update

--- 11-using-vllm/model/model.py
+++ 11-using-vllm/model/model.py
@@ -1,92 +1,91 @@
-import json
+import logging
+import os
 import subprocess
-import time
-from typing import Any, Dict, List
+import uuid
 
-import httpx  # Changed from aiohttp to httpx
+from transformers import AutoTokenizer
+from vllm import SamplingParams
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.engine.async_llm_engine import AsyncLLMEngine
+
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
+
+logger = logging.getLogger(__name__)
 
 
 class Model:
-    MAX_FAILED_SECONDS = 600  # 10 minutes; the reason this would take this long is mostly if we download a large model
-
-    def __init__(self, data_dir, config, secrets):
-        self._secrets = secrets
-        self._config = config
-        self.vllm_base_url = None
-
-        # TODO: uncomment for multi-GPU support
-        # command = "ray start --head"
-        # subprocess.check_output(command, shell=True, text=True)
+    def __init__(self, **kwargs):
+        self._config = kwargs["config"]
+        self.model = None
+        self.llm_engine = None
+        self.model_args = None
+        self.hf_secret_token = kwargs["secrets"]["hf_access_token"]
+        self.openai_compatible = self._config["model_metadata"]["openai_compatible"]
+        os.environ["HF_TOKEN"] = self.hf_secret_token
 
     def load(self):
-        self._client = httpx.AsyncClient(timeout=None)
 
-        self._vllm_config = self._config["model_metadata"]["arguments"]
-
-        command = ["python3", "-m", "vllm.entrypoints.openai.api_server"]
-        for key, value in self._vllm_config.items():
-            command.append(f"--{key.replace('_', '-')}")
-            command.append(str(value))
-
-        print(f"[DEBUG] Starting vLLM with command: {command}")
-
-        subprocess.Popen(command)
-
-        if "port" in self._vllm_config:
-            self._vllm_port = self._vllm_config["port"]
-        else:
-            self._vllm_port = 8000
-
-        self.vllm_base_url = f"http://localhost:{self._vllm_port}"
-
-        # Polling to check if the server is up
-        server_up = False
-        start_time = time.time()
-        while time.time() - start_time < self.MAX_FAILED_SECONDS:
-            try:
-                response = httpx.get(f"{self.vllm_base_url}/health")
-                if response.status_code == 200:
-                    server_up = True
-                    break
-            except httpx.RequestError:
-                time.sleep(1)  # Wait for 1 second before retrying
-
-        if not server_up:
-            raise RuntimeError(
-                "Server failed to start within the maximum allowed time."
+        try:
+            result = subprocess.run(
+                ["nvidia-smi"], capture_output=True, text=True, check=True
             )
+            print(result.stdout)
+        except subprocess.CalledProcessError as e:
+            print(f"Command failed with code {e.returncode}: {e.stderr}")
+        model_metadata = self._config["model_metadata"]
+        model_repo_id = model_metadata['repo_id']
+        vllm_engine_parameters = model_metadata["vllm_engine_parameters"]
+        logger.info(f"main model: {model_repo_id}")
+        logger.info(f"vllm engine parameters: {vllm_engine_parameters}")
+
+        self.model_args = AsyncEngineArgs(**vllm_engine_parameters)
+        if self.openai_compatible:
+            pass
+        else: 
+            self.llm_engine = AsyncLLMEngine.from_engine_args(self.model_args)
+        # create tokenizer for model to apply chat template to prompts
+        self.tokenizer = AutoTokenizer.from_pretrained(model_repo_id)
+
+        try:
+            result = subprocess.run(
+                ["nvidia-smi"], capture_output=True, text=True, check=True
+            )
+            print(result.stdout)
+        except subprocess.CalledProcessError as e:
+            print(f"Command failed with code {e.returncode}: {e.stderr}")
 
     async def predict(self, model_input):
+        if self.openai_compatible:
+            pass
+        else: 
+            prompt = model_input.pop("prompt")
+            stream = model_input.pop("stream", True)
 
-        # if the key metrics: true is present, let's return the vLLM /metrics endpoint
-        if model_input.get("metrics", False):
-            response = await self._client.get(f"{self.vllm_base_url}/metrics")
-            return response.text
-
-        # convenience for Baseten bridge
-        if "model" not in model_input and "model" in self._vllm_config:
-            print(
-                f"model_input missing model due to Baseten bridge, using {self._vllm_config['model']}"
+            sampling_params = SamplingParams(**model_input)
+            idx = str(uuid.uuid4().hex)
+            chat = [
+                {"role": "user", "content": prompt},
+            ]
+            # templatize the input to the model
+            input = self.tokenizer.apply_chat_template(
+                chat, tokenize=False, add_generation_prompt=True
             )
-            model_input["model"] = self._vllm_config["model"]
+            # since we accept any valid vllm sampling parameters, we can just pass it through
 
-        stream = model_input.get("stream", False)
-        if stream:
+            vllm_generator = self.llm_engine.generate(input, sampling_params, idx)
 
             async def generator():
-                async with self._client.stream(
-                    "POST",
-                    f"{self.vllm_base_url}/v1/chat/completions",
-                    json=model_input,
-                ) as response:
-                    async for chunk in response.aiter_bytes():
-                        if chunk:
-                            yield chunk
+                full_text = ""
+                async for output in vllm_generator:
+                    text = output.outputs[0].text
+                    delta = text[len(full_text) :]
+                    full_text = text
+                    yield delta
 
-            return generator()
-        else:
-            response = await self._client.post(
-                f"{self.vllm_base_url}/v1/chat/completions",
-                json=model_input,
-            )
-            return response.json()
+            if stream:
+                return generator()
+            else:
+                full_text = ""
+                async for delta in generator():
+                    full_text += delta
+                return {"text": full_text}
