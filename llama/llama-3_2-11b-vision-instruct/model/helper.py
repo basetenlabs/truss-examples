a5a60eff8f22e515f3e202c29e09ab0a370e2956
Derrick Yang <yangguangzheng122@gmail.com>
2024-09-27 15:17:40 -0700

    updated model and config

---
+++ llama/llama-3_2-11b-vision-instruct/model/helper.py
@@ -0,0 +1,57 @@
+import asyncio
+import logging
+import os
+import threading
+
+import httpx
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_HEALTH_CHECK_INTERVAL = 5  # seconds
+
+
+async def monitor_vllm_server_health(vllm_server_url, health_check_interval):
+    assert vllm_server_url is not None, "vllm_server_url must not be None"
+    try:
+        async with httpx.AsyncClient() as client:
+            while True:
+                response = await client.get(f"{vllm_server_url}/health")
+                if response.status_code != 200:
+                    raise RuntimeError("vLLM is unhealthy")
+                await asyncio.sleep(health_check_interval)
+    except Exception as e:
+        logging.error(
+            f"vLLM has gone into an unhealthy state due to error: {e}, restarting service now..."
+        )
+        os._exit(1)
+
+
+async def monitor_vllm_engine_health(vllm_engine, health_check_interval):
+    assert vllm_engine is not None, "vllm_engine must not be None"
+    try:
+        while True:
+            await vllm_engine.check_health()
+            await asyncio.sleep(health_check_interval)
+    except Exception as e:
+        logging.error(
+            f"vLLM has gone into an unhealthy state due to error: {e}, restarting service now..."
+        )
+        os._exit(1)
+
+
+def run_background_vllm_health_check(
+    use_openai_compatible_server=False,
+    health_check_interval=DEFAULT_HEALTH_CHECK_INTERVAL,
+    vllm_engine=None,
+    vllm_server_url=None,
+):
+    logger.info("Starting background health check loop")
+    loop = asyncio.new_event_loop()
+    if use_openai_compatible_server:
+        loop.create_task(
+            monitor_vllm_server_health(vllm_server_url, health_check_interval)
+        )
+    else:
+        loop.create_task(monitor_vllm_engine_health(vllm_engine, health_check_interval))
+    thread = threading.Thread(target=loop.run_forever, daemon=True)
+    thread.start()
--- llama/llama-3_2-11b-vision-instruct/model/model.py
+++ llama/llama-3_2-11b-vision-instruct/model/model.py
@@ -1,32 +1,234 @@
-"""
-The `Model` class is an interface between the ML model that you're packaging and the model
-server that you're running it on.
+import logging
+import os
+import subprocess
+import time
+import uuid
 
-The main methods to implement here are:
-* `load`: runs exactly once when the model server is spun up or patched and loads the
-   model onto the model server. Include any logic for initializing your model, such
-   as downloading model weights and loading the model into memory.
-* `predict`: runs every time the model server is called. Include any logic for model
-  inference and return the model output.
+import httpx
+from model.helper import run_background_vllm_health_check
+from transformers import AutoTokenizer
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.engine.async_llm_engine import AsyncLLMEngine
 
-See https://truss.baseten.co/quickstart for more.
-"""
+from vllm import SamplingParams
+
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
+
+logger = logging.getLogger(__name__)
 
 
 class Model:
-    def __init__(self, **kwargs):
-        # Uncomment the following to get access
-        # to various parts of the Truss config.
+    # TODO: better way to detect model server startup failure than using `MAX_FAILED_SECONDS`
+    MAX_FAILED_SECONDS = 1500  # 25 minutes; the reason this would take this long is mostly if we download a large model
+    HEALTH_CHECK_INTERVAL = 5  # seconds
 
-        # self._data_dir = kwargs["data_dir"]
-        # self._config = kwargs["config"]
-        # self._secrets = kwargs["secrets"]
-        self._model = None
+    def __init__(self, **kwargs):
+        self._config = kwargs["config"]
+        self.model_id = None
+        self.llm_engine = None
+        self.model_args = None
+        self.hf_secret_token = kwargs["secrets"].get("hf_access_token", None)
+        self.openai_compatible = self._config["model_metadata"].get(
+            "openai_compatible", False
+        )
+        self.vllm_base_url = None
+        os.environ["HF_TOKEN"] = self.hf_secret_token
 
     def load(self):
-        # Load model here and assign to self._model.
-        pass
+        self._model_metadata = self._config["model_metadata"]
+        self._model_repo_id = self._model_metadata["repo_id"]
+        self._vllm_config = self._model_metadata["vllm_config"]
+        if self._vllm_config is None:
+            self._vllm_config = {}
+        logger.info(f"main model: {self._model_repo_id}")
+        logger.info(f"vllm config: {self._vllm_config}")
+        if self.openai_compatible:
+            self._client = httpx.AsyncClient(timeout=None)
+            command = ["vllm", "serve", self._model_repo_id]
+            for key, value in self._vllm_config.items():
+                if value is True:
+                    command.append(f"--{key.replace('_', '-')}")
+                elif value is False:
+                    continue
+                elif isinstance(value, dict):
+                    nested_config = ",".join(
+                        f"{sub_key.replace('_', '-') }={sub_value}" for sub_key, sub_value in value.items()
+                    )
+                    command.append(f"--{key.replace('_', '-')}")
+                    command.append(nested_config)
+                else:
+                    command.append(f"--{key.replace('_', '-')}")
+                    command.append(str(value))
 
-    def predict(self, model_input):
-        # Run model inference here
-        return model_input
+            logger.info(
+                f"Starting openai compatible vLLM server with command: {command}"
+            )
+
+            try: 
+                self._vllm_process = subprocess.Popen(
+                    command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+                )
+            except Exception as e:
+                logger.error(f"VLLM process exception: {e}")
+                raise RuntimeError(f"VLLM Command failed with error: {e}")
+
+            # Wait for 10 seconds and check if command fails
+            time.sleep(10)
+
+            if self._vllm_process.poll() is None:
+                logger.info("Command to start vLLM server ran successfully")
+            else:
+                stdout, stderr = self._vllm_process.communicate()
+                if self._vllm_process.returncode != 0:
+                    logger.error(f"Command failed with error: {stderr}")
+                    raise RuntimeError(
+                        f"Command failed with code {self._vllm_process.returncode}: {stderr}"
+                    )
+            stdout, stderr = self._vllm_process.communicate()
+            logger.info(f"VLLM server output: {stdout}")
+            logger.info(f"VLLM server error: {stderr}")
+
+            if self._vllm_config and "port" in self._vllm_config:
+                self._vllm_port = self._vllm_config["port"]
+            else:
+                self._vllm_port = 8000
+
+            self.vllm_base_url = f"http://localhost:{self._vllm_port}"
+            
+            print(self.vllm_base_url)
+
+            # Polling to check if the server is up
+            server_up = False
+            start_time = time.time()
+            while time.time() - start_time < self.MAX_FAILED_SECONDS:
+                try:
+                    response = httpx.get(f"{self.vllm_base_url}/health")
+                    logger.info(f"Checking server health: {response.status_code}")
+                    if response.status_code == 200:
+                        server_up = True
+                        break
+                except httpx.RequestError as e:
+                    seconds_passed = int(time.time() - start_time)
+                    if seconds_passed % 10 == 0:
+                        logger.info(
+                            f"Server is starting for {seconds_passed} seconds: {e}"
+                        )
+                    time.sleep(1)  # Wait for 1 second before retrying
+
+            if not server_up:
+                raise RuntimeError(
+                    "Server failed to start within the maximum allowed time."
+                )
+        else:
+            try:
+                result = subprocess.run(
+                    ["nvidia-smi"], capture_output=True, text=True, check=True
+                )
+                logger.info(result.stdout)
+            except subprocess.CalledProcessError as e:
+                logger.error(f"Command failed with code {e.returncode}: {e.stderr}")
+
+            self.model_args = AsyncEngineArgs(
+                model=self._model_repo_id, **self._vllm_config
+            )
+            self.llm_engine = AsyncLLMEngine.from_engine_args(
+                engine_args=self.model_args
+            )
+            self.tokenizer = AutoTokenizer.from_pretrained(self._model_repo_id)
+
+            try:
+                result = subprocess.run(
+                    ["nvidia-smi"], capture_output=True, text=True, check=True
+                )
+                logger.info(result.stdout)
+            except subprocess.CalledProcessError as e:
+                logger.error(f"Command failed with code {e.returncode}: {e.stderr}")
+        try:
+            run_background_vllm_health_check(
+                self.openai_compatible,
+                self.HEALTH_CHECK_INTERVAL,
+                self.llm_engine,
+                self.vllm_base_url,
+            )
+        except Exception as e:
+            raise RuntimeError(f"Failed to start background health check: {e}")
+
+    async def predict(self, model_input):
+        if "messages" not in model_input and "prompt" not in model_input:
+            raise ValueError("Prompt or messages must be provided")
+
+        stream = model_input.get("stream", False)
+        if self.openai_compatible:
+            # if the key metrics: true is present, let's return the vLLM /metrics endpoint
+            if model_input.get("metrics", False):
+                response = await self._client.get(f"{self.vllm_base_url}/metrics")
+                return response.text
+
+            # convenience for Baseten bridge
+            if "model" not in model_input and self._model_repo_id:
+                logger.info(
+                    f"model_input missing model due to Baseten bridge, using {self._model_repo_id}"
+                )
+                model_input["model"] = self._model_repo_id
+
+            if stream:
+                async def generator():
+                    async with self._client.stream(
+                        "POST",
+                        f"{self.vllm_base_url}/v1/chat/completions",
+                        json=model_input,
+                    ) as response:
+                        async for chunk in response.aiter_bytes():
+                            if chunk:
+                                yield chunk
+
+                return generator()
+            else:
+                response = await self._client.post(
+                    f"{self.vllm_base_url}/v1/chat/completions",
+                    json=model_input,
+                )
+                return response.json()
+        else:
+            # SamplingParams does not take/use argument 'model'
+            if "model" in model_input:
+                model_input.pop("model")
+            if "prompt" in model_input:
+                prompt = model_input.pop("prompt")
+                sampling_params = SamplingParams(**model_input)
+                idx = str(uuid.uuid4().hex)
+                messages = [
+                    {"role": "user", "content": prompt},
+                ]
+                # templatize the input to the model
+                input = self.tokenizer.apply_chat_template(
+                    messages, tokenize=False, add_generation_prompt=True
+                )
+            elif "messages" in model_input:
+                messages = model_input.pop("messages")
+                sampling_params = SamplingParams(**model_input)
+                idx = str(uuid.uuid4().hex)
+                # templatize the input to the model
+                input = self.tokenizer.apply_chat_template(
+                    messages,
+                    tokenize=False,
+                )
+            logger.info(f"Using SamplingParams: {sampling_params}")
+            # since we accept any valid vllm sampling parameters, we can just pass it through
+            vllm_generator = self.llm_engine.generate(input, sampling_params, idx)
+
+            async def generator():
+                full_text = ""
+                async for output in vllm_generator:
+                    text = output.outputs[0].text
+                    delta = text[len(full_text) :]
+                    full_text = text
+                    yield delta
+
+            if stream:
+                return generator()
+            else:
+                full_text = ""
+                async for delta in generator():
+                    full_text += delta
+                return {"text": full_text}
