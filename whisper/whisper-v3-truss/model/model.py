22f5d48a2c79e09d45552e2e00c728d30c216432
Het Trivedi <het.trivedi05@gmail.com>
2024-10-07 15:41:49 -0700

    fixing indentation

--- whisper/whisper-v3-truss/model/model.py
+++ whisper/whisper-v3-truss/model/model.py
@@ -16,29 +16,27 @@ class Model:
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model = None
 
-        def audio_url_to_waveform(self, path_or_url: str):
-            sampling_rate = 16000
-            # Use ffmpeg to read the audio file and convert to the monochannel, 16kHz
-            out, _ = (
-                ffmpeg.input(
-                    path_or_url, seekable=0
-                )  # Disable HTTP seekable (range requests)
-                .output(
-                    "pipe:", format="wav", acodec="pcm_s16le", ac=1, ar=sampling_rate
-                )
-                .run(capture_stdout=True, capture_stderr=True)
-            )
+    def audio_url_to_waveform(self, path_or_url: str):
+        sampling_rate = 16000
+        # Use ffmpeg to read the audio file and convert to the monochannel, 16kHz
+        out, _ = (
+            ffmpeg.input(
+                path_or_url, seekable=0
+            )  # Disable HTTP seekable (range requests)
+            .output("pipe:", format="wav", acodec="pcm_s16le", ac=1, ar=sampling_rate)
+            .run(capture_stdout=True, capture_stderr=True)
+        )
 
-            # Convert the raw byte data into a numpy array
-            waveform_np = np.frombuffer(out, dtype=np.int16)
+        # Convert the raw byte data into a numpy array
+        waveform_np = np.frombuffer(out, dtype=np.int16)
 
-            # Normalize the waveform data
-            waveform_np = waveform_np.astype(np.float32) / 32768.0
+        # Normalize the waveform data
+        waveform_np = waveform_np.astype(np.float32) / 32768.0
 
-            # Convert the numpy array to a pytorch tensor
-            waveform_tensor = torch.tensor(waveform_np, dtype=torch.float32)
+        # Convert the numpy array to a pytorch tensor
+        waveform_tensor = torch.tensor(waveform_np, dtype=torch.float32)
 
-            return waveform_tensor
+        return waveform_tensor
 
     def load(self):
         self.model = whisper.load_model(
