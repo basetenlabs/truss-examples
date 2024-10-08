457714ab6ee348e4b7686b92347290ea053ed742
Het Trivedi <het.trivedi05@gmail.com>
2024-10-08 10:27:50 -0700

    Updating code for faster whisper

--- whisper/faster-whisper-v2/model/model.py
+++ whisper/faster-whisper-v2/model/model.py
@@ -1,11 +1,13 @@
 import base64
 import logging
-import os
 import tempfile
 import time
 from typing import Dict
-
+import numpy as np
+import torch
 import httpx
+import ffmpeg
+import io
 from faster_whisper import BatchedInferencePipeline, WhisperModel
 
 DEFAULT_BATCH_SIZE = 8
@@ -30,11 +32,60 @@ class Model:
     def base64_to_wav(self, base64_string):
         binary_data = base64.b64decode(base64_string)
         with tempfile.NamedTemporaryFile(
-            suffix=".wav", delete=False
+                suffix=".wav", delete=False
         ) as output_file_path:
             output_file_path.write(binary_data)
             output_file_path.flush()
         return output_file_path.name
+
+    def audio_url_to_waveform(self, path_or_url: str):
+        sampling_rate = 16000
+        # Use ffmpeg to read the audio file and convert to the monochannel, 16kHz
+        out, _ = (
+            ffmpeg.input(
+                path_or_url, seekable=0
+            )  # Disable HTTP seekable (range requests)
+            .output("pipe:", format="wav", acodec="pcm_s16le", ac=1, ar=sampling_rate)
+            .run(capture_stdout=True, capture_stderr=True)
+        )
+
+        # Convert the raw byte data into a numpy array
+        waveform_np = np.frombuffer(out, dtype=np.int16)
+
+        # Normalize the waveform data
+        waveform_np = waveform_np.astype(np.float32) / 32768.0
+
+        # Convert the numpy array to a pytorch tensor
+        waveform_tensor = torch.tensor(waveform_np, dtype=torch.float32)
+
+        return waveform_tensor
+
+    def base64_to_waveform(self, base64_audio: str):
+        sampling_rate = 16000
+
+        # Decode the Base64 string to get the raw audio bytes
+        audio_bytes = base64.b64decode(base64_audio)
+
+        # Convert the raw audio bytes to a binary stream for ffmpeg to process
+        audio_stream = io.BytesIO(audio_bytes)
+
+        # Use ffmpeg to process the raw bytes and convert to monochannel, 16kHz wav format
+        out, _ = (
+            ffmpeg.input('pipe:0')  # Input is from pipe
+            .output("pipe:", format="wav", acodec="pcm_s16le", ac=1, ar=sampling_rate)
+            .run(input=audio_stream.read(), capture_stdout=True, capture_stderr=True)
+        )
+
+        # Convert the raw byte data into a numpy array
+        waveform_np = np.frombuffer(out, dtype=np.int16)
+
+        # Normalize the waveform data
+        waveform_np = waveform_np.astype(np.float32) / 32768.0
+
+        # Convert the numpy array to a pytorch tensor
+        waveform_tensor = torch.tensor(waveform_np, dtype=torch.float32)
+
+        return waveform_tensor
 
     def download_file(self, url):
         with httpx.Client() as client:
@@ -42,7 +93,7 @@ class Model:
             if response.status_code == 200:
                 # Save the file to a local file
                 with tempfile.NamedTemporaryFile(
-                    suffix=".wav", delete=False
+                        suffix=".wav", delete=False
                 ) as output_file_path:
                     output_file_path.write(response.content)
                     output_file_path.flush()
@@ -79,16 +130,19 @@ class Model:
             }
 
         if audio_base64:
-            file_name = self.base64_to_wav(audio_base64)
-            response["audio"] = file_name
+            waveform = self.base64_to_waveform(audio_base64)
+            response["audio"] = waveform
+            # file_name = self.base64_to_wav(audio_base64)
+            # response["audio"] = file_name
 
         elif url:
             start = time.time()
-            file_name = self.download_file(url)
+            # file_name = self.download_file(url)
+            waveform = self.audio_url_to_waveform(url)
             logging.info(
                 f"url download time: {time.time() - start}",
             )
-            response["audio"] = file_name
+            response["audio"] = waveform
 
         response["word_timestamps"] = word_level_timestamps
         response["initial_prompt"] = prompt
@@ -107,39 +161,33 @@ class Model:
         full_transcript = ""
         audio = model_input.pop("audio")
 
-        if audio:
-            segments, info = self.batched_model.transcribe(audio, **model_input)
-            for segment in segments:
-                segment_information = {
-                    "text": segment.text,
-                    "start": segment.start,
-                    "end": segment.end,
-                }
-
-                words = []
-                if segment.words:
-                    for word in segment.words:
-                        words.append(
-                            {"start": word.start, "end": word.end, "word": word.word}
-                        )
-
-                    segment_information["words"] = words
-
-                all_segments.append(segment_information)
-                full_transcript += segment.text
-
-            language = info.language
-            end = time.time()
-            transcription_time = end - start
-
-            # Delete temp file
-            os.remove(audio)
-
-            return {
-                "segments": all_segments,
-                "language": language,
-                "transcript": full_transcript,
-                "transcription_time": transcription_time,
+        segments, info = self.batched_model.transcribe(audio, **model_input)
+        for segment in segments:
+            segment_information = {
+                "text": segment.text,
+                "start": segment.start,
+                "end": segment.end,
             }
 
-        return model_input
+            words = []
+            if segment.words:
+                for word in segment.words:
+                    words.append(
+                        {"start": word.start, "end": word.end, "word": word.word}
+                    )
+
+                segment_information["words"] = words
+
+            all_segments.append(segment_information)
+            full_transcript += segment.text
+
+        language = info.language
+        end = time.time()
+        transcription_time = end - start
+
+        return {
+            "segments": all_segments,
+            "language": language,
+            "transcript": full_transcript,
+            "transcription_time": transcription_time,
+        }
