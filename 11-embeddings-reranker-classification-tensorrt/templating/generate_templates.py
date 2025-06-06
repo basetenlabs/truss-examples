9d86ba780da6de776b9df732a8c23ff810d0aee1
michaelfeil <michael.feil@baseten.co>
2025-06-06 21:16:06 +0000

    add qwen3 model for embeddings

--- 11-embeddings-reranker-classification-tensorrt/templating/generate_templates.py
+++ 11-embeddings-reranker-classification-tensorrt/templating/generate_templates.py
@@ -89,6 +89,16 @@ With BEI you get the following benefits:
             if isinstance(dp.task, Predictor)
             else "/rerank"
         )
+        overrides_engine_builder = os.environ.get("ENGINE_BUILDER_OVERRIDES")
+        overrides_briton = os.environ.get("BEI_OVERRIDES")
+
+        if overrides_engine_builder is not None or overrides_briton is not None:
+            version_overrides = VersionsOverrides(
+                engine_builder_version=overrides_engine_builder,
+                bei_version=overrides_briton,
+            )
+        else:
+            version_overrides = None
         return TrussConfig(
             model_metadata=dp.task.model_metadata,
             trt_llm=TRTLLMConfiguration(
@@ -113,6 +123,7 @@ With BEI you get the following benefits:
                 runtime=TrussTRTLLMRuntimeConfiguration(
                     webserver_default_route=endpoint,
                 ),
+                version_overrides=version_overrides,
             ),
             resources=Resources(
                 accelerator=dp.accelerator,
@@ -933,6 +944,49 @@ DEPLOYMENTS_BEI = [
         Embedder(),
         solution=BEI(make_fp8=True),
     ),
+    Deployment(
+        "Qwen/Qwen3-Embedding-8B",
+        "Qwen/Qwen3-Embedding-8B",
+        Accelerator.H100_40GB,
+        Embedder(),
+        solution=BEI(make_fp8=True),
+    ),
+    Deployment(
+        "Qwen/Qwen3-Embedding-4B",
+        "Qwen/Qwen3-Embedding-4B",
+        Accelerator.H100_40GB,
+        Embedder(),
+        solution=BEI(make_fp8=True),
+    ),
+    Deployment(
+        "Qwen/Qwen3-Embedding-0.6B",
+        "Qwen/Qwen3-Embedding-0.6B",
+        Accelerator.L4,
+        Embedder(),
+        solution=BEI(make_fp8=True),
+    ),
+    Deployment(
+        "Qwen/Qwen3-Reranker-0.6B",
+        "Qwen/Qwen3-Reranker-0.6B",
+        Accelerator.L4,
+        Embedder(),
+        solution=BEI(make_fp8=True),
+    ),
+    Deployment(
+        "Qwen/Qwen3-Reranker-4B",
+        "Qwen/Qwen3-Reranker-4B",
+        Accelerator.H100_40GB,
+        Embedder(),
+        solution=BEI(make_fp8=True),
+    ),
+    Deployment(
+        "Qwen/Qwen3-Reranker-8B",
+        "Qwen/Qwen3-Reranker-8B",
+        Accelerator.H100_40GB,
+        Embedder(),
+        solution=BEI(make_fp8=True),
+    ),
+    
 ]
 
 DEPLOYMENTS_HFTEI = [  # models that don't yet run on BEI
